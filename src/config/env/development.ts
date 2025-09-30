6c3af9ecd756b251ba387910ed70067a16b24a16
cj-prince <davids.chijioke@gmail.com>
2025-09-30 12:22:25 +0100

    package json update

--- src/config/env/development.ts
+++ src/config/env/development.ts
@@ -4,7 +4,7 @@ configDotenv();
 const development = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.TEST_PORT,
-  DATABASE_URL: process.env.TEST_DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   SALT_ROUND: process.env.TEST_SALT_ROUND,
 
